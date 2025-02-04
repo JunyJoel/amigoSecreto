ef2adc62d744b3c588b3393783a7993b8015c730
JunyJoel <juny.joel@gmail.com>
2025-02-04 13:56:07 -0600

    agregando funciones en js

--- AmigoSecretoLOGIC.js
+++ AmigoSecretoLOGIC.js
@@ -1,1 +1,23 @@
 
+let listaAmigos = [];
+//console.log("hola consola");
+
+function agregarAmigo(){
+    
+    let nombre = document.getElementById("amigo").value;
+    nombre = nombre.toUpperCase();
+
+    if(nombre==""){
+        alert("Coloca un nombre");
+    }else{
+    console.log(`este es el nombre: ${nombre}`);
+    listaAmigos.push(nombre);
+    console.log(listaAmigos);
+    }
+}
+
+function sortearAmigo(){
+    let randomNumber = Math.floor(Math.random()*listaAmigos.length+1)
+    let amigoSecreto = listaAmigos[randomNumber];
+    alert(`Tu amigo secreto es ${amigoSecreto}`);
+}
