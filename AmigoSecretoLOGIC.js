ff7464ba38a8a4f911557fd54d9fd4fc4135a540
JunyJoel <juny.joel@gmail.com>
2025-02-04 14:52:02 -0600

    diseño y logica funcional

--- AmigoSecretoLOGIC.js
+++ AmigoSecretoLOGIC.js
@@ -1,6 +1,7 @@
 
 let listaAmigos = [];
-//console.log("hola consola");
+let listaNumerosSorteados = [];
+let amigoSecreto = "";
 
 function agregarAmigo(){
     
@@ -10,14 +11,46 @@ function agregarAmigo(){
     if(nombre==""){
         alert("Coloca un nombre");
     }else{
-    console.log(`este es el nombre: ${nombre}`);
-    listaAmigos.push(nombre);
-    console.log(listaAmigos);
+        if(listaAmigos.includes(nombre)){
+            return agregarAmigo();
+        }else{
+            listaAmigos.push(nombre);
+            document.getElementById("amigo").value = "";
+            console.log(listaAmigos);
+        }
     }
 }
 
 function sortearAmigo(){
-    let randomNumber = Math.floor(Math.random()*listaAmigos.length+1)
-    let amigoSecreto = listaAmigos[randomNumber];
-    alert(`Tu amigo secreto es ${amigoSecreto}`);
+    let randomNumber = Math.floor(Math.random()*listaAmigos.length)
+    //console.log(`listaAmigos = ${listaAmigos.length} elementos`);
+    
+    if(listaNumerosSorteados.length == listaAmigos.length){
+        //asignarTextoElemento('p','¡Ya se terminaron los NOMBRES!')
+        inicializar();
+        alert("Se terminaron los nombres");
+    } else {
+        //si el numero generado esta incluido en la lista
+        if(listaNumerosSorteados.includes(randomNumber)){
+            return sortearAmigo();
+        }else{//si no esta en la lista, agregarlo
+            listaNumerosSorteados.push(randomNumber);
+            amigoSecreto = listaAmigos[randomNumber];
+            textoElemento("miH2",`Tu amigo secreto es ${amigoSecreto}`);
+            return randomNumber;
+        }
+    }
+}
+
+function textoElemento(id, texto){
+    let elementoHTML = document.getElementById(id);
+    elementoHTML.innerHTML = texto;
+    return elementoHTML;
+}
+
+function inicializar(){
+    textoElemento("miH2",`Digite el nombre de sus amigos`);
+    listaAmigos = [];
+    listaNumerosSorteados = [];
+    amigoSecreto = "";
 }
